9442bd76218541923cdb5b835f8b8f393f9adce9
Stephen Vinuya <stephen.vinuya@ffuf.de>
2018-06-20 00:02:28 +0800

    Added enemies

--- game/game-manager.js
+++ game/game-manager.js
@@ -4,17 +4,24 @@ class GameManager {
         const playerPosition = createVector(width / 2, height - 50);
         const playerSize = createVector(50,50);
         this.player = new Player(playerPosition,3,playerSize);
+        this.enemies = [new Enemy(createVector(50,50), 3, playerSize)]; 
     }
 
     render() {
         this.playerRender();
+        this.enemyRender();
     }
-
-
 
     playerRender() {
         this.player.update();
         this.player.render();
+    }
+
+    enemyRender() {
+        this.enemies.forEach(enemy => {
+            enemy.update();
+            enemy.render();
+        });
     }
 
 }
--- game/game-objects/enemy.js
+++ game/game-objects/enemy.js
@@ -1,1 +1,31 @@
-class
+class Enemy {
+
+    constructor(position, life, size) {
+        this.position = position;
+        this.life = life;
+        this.size = size;
+    }
+
+    render() {
+        const halfSize = this.size.x / 2;
+        const quaterSize = (this.size.x / 4) + 2;
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(frameCount / 10);    
+        rectMode(CENTER);
+        fill(200);
+        rect(-quaterSize, quaterSize, halfSize, halfSize);
+        rect(quaterSize, -quaterSize, halfSize, halfSize);
+        rect(-quaterSize, -quaterSize, halfSize, halfSize);
+        rect(quaterSize, quaterSize, halfSize, halfSize);
+        fill(250,100,100);
+        ellipseMode(CENTER);
+        ellipse(0, 0,halfSize, halfSize);
+        pop();
+    }
+
+    update() {
+
+    }
+
+}
